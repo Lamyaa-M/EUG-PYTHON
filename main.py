46a54e4f4344417f475ed5260e4c5427074f206a
Lamyaa <mlarboul@student.42.fr>
2025-10-02 13:12:58 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+# TODO: Create an integer variable named 'age' with your age
+# TODO: Create a float variable named 'height' with your height in meters
+# TODO: Create a string variable named 'name' with your name
+# TODO: Create a boolean variable named 'is_student' and set it to True
+# Print all variables and their types
+# Example: print(f"Age: {age}, Type: {type(age)}")
+age = 23
+height = 1.64
+name = "lamyaa"
+is_student = True
+# print(age)
+# print(height)
+# print(name)
+# print(is_student)
+# print(f"Age: {age}, Type: {type(age)}")
+# print(f"height: {height}, Type: {type(height)}")
+# print(f"name: {name}, Type: {type(name)}")
+# print(f"is_student: {is_student}, Type: {type(is_student)}")
+a = 16
+b = 9
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a % b)
+# print(a ** b)
+first_name = "lamyaa"
+last_name = "MARZOUQ"
+full_name = first_name + " " + last_name
+# print(full_name.upper())
+# print(len(full_name))
+sentence = "first course of python"
+words = sentence.split()
+# print(words)
+num_str = "205"
+num_int = int(num_str)
+# print(type(num_int))
+num_float = 14.9
+num_int2 = int(num_float)
+# print(type(num_int2))
+number = 2
+number_float = float(number)
+# print(type(number_float))
+# print(number_float)
+str1 = str(number_float)
+# print(str1, type(str1))
+rep1 = True
+rep2 = False
+# print("rep1 AND rep2:", rep1 and rep2)
+# print("rep1 or rep2:", rep1 or rep2)
+# print("not rep1:", not rep1)
+number1 = 200
+number2 = 250
+# print("number1 == number2:", number1 == number2)
+# print("number1 != number2:", number1 != number2)
+# print("number1 > number2:", number1 > number2)
+# print("number1 < number2:", number1 < number2)
+# print("number1 >= number2:", number1 >= number2)
+# print("number1 <= number2:", number1 <= number2)
+favorite_fruits = ['strawberry', 'blueberry', 'mango', 'kiwi']
+# print(favorite_fruits)
+favorite_fruits.append('apple')
+# print(favorite_fruits)
+favorite_fruits.pop(1)
+# print(favorite_fruits)
+favorite_fruits.sort()
+# print(favorite_fruits)
+first_three_fruits = favorite_fruits[:3]
+# print(first_three_fruits)
+print("Original list:", favorite_fruits)
+print("New list (first three fruits):", first_three_fruits)
